058f20147f158f30be8d03f2774de02f1d7f9829
JAY PONDA <77849303+JayPonda@users.noreply.github.com>
2022-02-27 03:10:06 +0530

    add necessary comments

--- tree/Tree.py
+++ tree/Tree.py
@@ -10,11 +10,14 @@ class Tree:
         self.root.setWord()
 
 
-    # get and print methods
+    # get and set methods
 
     def getWordset(self):
         return self.wordset
 
+    # it is not for users ...
+    # --- start ---
+    # you can use to check last word of your character is true or not
     def getPreord(self):
         nodeorder = []
         Stack = deque([])
@@ -42,6 +45,7 @@ class Tree:
 
         return [(i.getTitle(), i.getWord()) for i in nodeorder]
 
+    # this is also use to visulize your tree -> it returns list
     def getTree(self):
         def TreeList(obj):
             return (
@@ -51,25 +55,35 @@ class Tree:
         return TreeList(self.root)[1]
 
 
+    # --- end ---
+    
     # add word
-
     def addWord(self, word):
         
+        # if word is avalable then not to add 
+        # to decrease the complexity you can find word directly on tree (but i am not going in that way here but algorithem is coverd partially in this method)
         if word  not in self.wordset:
             self.wordset.append(word)
 
+            # starting with root
             currentNode = self.root
+            
+            # traversing word letter by letter 
             for letter in word:
 
+                # if child not exist then return -1 else index
                 ind = currentNode.getChildIndex(letter)
 
+                # if word not exist as a child of nood then create new node for letter
                 if ind == -1:
-                    newnode = nd.Node(letter)
-                    currentNode.addChild(newnode)
-                    currentNode = newnode
+                    newnode = nd.Node(letter)     # create newnode
+                    currentNode.addChild(newnode) # add newnode to current child
+                    currentNode = newnode         # update current node as newnode
                 else:
+                    # update current node as current node's (ind)'th child  
                     currentNode = (currentNode.getChild())[ind]
 
+            # word is complete then set isword attribute as true
             currentNode.setWord(True)
 
 
@@ -79,8 +93,21 @@ class Tree:
         self.root.clearChild()
 
     def deleteWord(self, word):
-        # case 1: word is substring of tree -> change isword = False
-        # case 2: word is goint towords leaf -> last disting obj -> clearchild
+        # case 1: word is substring of tree -> change isword = False 
+        # suppose two words in tree are [top, to] -> status of tree [(t, false, [...child]), (o, true, [...child]), (p, true, [...child])]
+        # just change (o, true, [...child]) to (o, false, [...child]) so it's no more recognizing as word
+        # delete from wordset
+        # (*, true) -> (t, false) -> (o, true) -> (o, true)
+        # (*, true) -> (t, false) -> (o, false) -> (o, true)
+        
+        # case 2: word is goint towords leaf -> last indipendent obj -> clearchild
+        # suppose two words in tree are [top, toor] -> status of tree [(t, false, [...child]), (o, true, [...child]), (o, false, [...child]),(p, true, [...child]), (r, true, [...child]) ]
+        # go to last indipendent node is o(after t) so delete (ind)'th child of o 
+        #           | remove 1'st child from this o
+        # * -> t -> o -> p
+        #            \
+        #             o -> r
+        
         if word not in self.wordset:
             return -1, f'{word} is not avalable in tree'
 
@@ -120,10 +147,18 @@ class Tree:
      
 
     # suggest word
-
+    # logic is divided in two parts
+    # 1. reach to the last cheracter of the string
+    # 2. travers from that in child and add to ans
+    # * -> t -> r -> u -> e
+    #       \         \
+    #        \         c -> k
+    #         o -> p -> s
+    # word is tru ->1. reach to u
+    # 2. traverse in child (tru) + e and (tru) + c + k is two suggestion
+    
     def suggestWord(self, pathstack, char, n = 3):
         ans = []
-        # ans = [], char = t
         # pathstack = [(self.root, ""), ]
 
         def traversetree(tobj):
@@ -177,14 +212,7 @@ class Tree:
 
         return ans, pathstack
 
-            
-                    
-
-
-        
-
-        
-
+     # tack input by char
     def suggestAndAdd(self):
         word = ""
         char = ""
@@ -196,18 +224,23 @@ class Tree:
             if not isaddword:
                 k = self.suggestWord([(t.root, "")] if word == "" else k[1], char)
 
+                # if suggestion avalable then print, if not than isaddword true so not to suggest word
                 if k[0] :
                     for i in k[0]:
                         print("- ",i)
                 else:
                     isaddword = True
 
+            # concatinate char and make word
             word += char
 
-        self.addWord(word[:-1:1])
+        # add word
+        if isaddword:
+            self.addWord(word[:-1:1])
         return self.getWordset()
             
 
+# some test cases to check functionality
 if __name__ == "__main__":
     t = Tree()
     t.addWord('too')
